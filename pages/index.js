6b99a2361851e1ddc18899ca2f95fd3a65a7358b
DarkAstaroth <mninahuanca4@gmail.com>
2021-12-16 09:19:10 -0400

    Diseño inicial para el frontend de la aplicacion

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+import Layout from "../components/Layout";
+
+export default function Home() {
+  return (
+    <>
+      <Layout>
+	<h1 className="text-2xl text-gray-800 font-light" >Clientes</h1>
+      </Layout>
+    </>
+  );
+}
